5be07bdf280dd28dce83d32429c114a9c50d8e75
mriart <marc_riart@es.ibm.com>
2024-03-02 11:42:23 +0100

    Dita of today

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"math/rand/v2"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 )
@@ -77,19 +78,20 @@ const (
 )
 
 var (
-	gen  = []string{}
-	feb  = []string{}
-	mar  = []string{}
-	apr  = []string{}
-	may  = []string{}
-	jun  = []string{}
-	jul  = []string{}
-	aug  = []string{}
-	sep  = []string{}
-	oct  = []string{}
-	nov  = []string{}
-	dec  = []string{}
-	none = []string{}
+	gen    = []string{}
+	feb    = []string{}
+	mar    = []string{}
+	apr    = []string{}
+	may    = []string{}
+	jun    = []string{}
+	jul    = []string{}
+	aug    = []string{}
+	sep    = []string{}
+	oct    = []string{}
+	nov    = []string{}
+	dec    = []string{}
+	none   = []string{}
+	todays = []string{}
 
 	noMonth  bool
 	noSeason bool
@@ -104,6 +106,10 @@ func init() {
 	}
 	defer file.Close()
 
+	// Create a regular expression for the particular dites of the day like Sant Jordi, Nadal...
+	pattern := `(sant jordi|sant joan|tots sants|nadal|\([0-9])`
+	re := regexp.MustCompile(pattern)
+
 	// Create a new scanner
 	scanner := bufio.NewScanner(file)
 
@@ -111,50 +117,51 @@ func init() {
 		noMonth = false
 		noSeason = false
 		line := scanner.Text()
+		lineLower := strings.ToLower(line)
 
 		switch {
-		case strings.Contains(strings.ToLower(line), "gener"):
+		case strings.Contains(lineLower, "gener"):
 			gen = append(gen, line)
-		case strings.Contains(strings.ToLower(line), "febrer"):
+		case strings.Contains(lineLower, "febrer"):
 			feb = append(feb, line)
-		case strings.Contains(strings.ToLower(line), "març"):
+		case strings.Contains(lineLower, "març"):
 			mar = append(mar, line)
-		case strings.Contains(strings.ToLower(line), "abril"):
+		case strings.Contains(lineLower, "abril"):
 			apr = append(apr, line)
-		case strings.Contains(strings.ToLower(line), "maig"):
+		case strings.Contains(lineLower, "maig"):
 			may = append(may, line)
-		case strings.Contains(strings.ToLower(line), "juny"):
+		case strings.Contains(lineLower, "juny"):
 			jun = append(jun, line)
-		case strings.Contains(strings.ToLower(line), "juliol"):
+		case strings.Contains(lineLower, "juliol"):
 			jul = append(jul, line)
-		case strings.Contains(strings.ToLower(line), "agost"):
+		case strings.Contains(lineLower, "agost"):
 			aug = append(aug, line)
-		case strings.Contains(strings.ToLower(line), "setembre"):
+		case strings.Contains(lineLower, "setembre"):
 			sep = append(sep, line)
-		case strings.Contains(strings.ToLower(line), "octubre"):
+		case strings.Contains(lineLower, "octubre"):
 			oct = append(oct, line)
-		case strings.Contains(strings.ToLower(line), "novembre"):
+		case strings.Contains(lineLower, "novembre"):
 			nov = append(nov, line)
-		case strings.Contains(strings.ToLower(line), "desembre"):
+		case strings.Contains(lineLower, "desembre"):
 			dec = append(dec, line)
 		default:
 			noMonth = true
 		}
 
 		switch {
-		case strings.Contains(strings.ToLower(line), "hivern"):
+		case strings.Contains(lineLower, "hivern"):
 			dec = append(dec, line)
 			gen = append(gen, line)
 			feb = append(feb, line)
-		case strings.Contains(strings.ToLower(line), "primavera"):
+		case strings.Contains(lineLower, "primavera"):
 			mar = append(mar, line)
 			apr = append(apr, line)
 			may = append(may, line)
-		case strings.Contains(strings.ToLower(line), "estiu"):
+		case strings.Contains(lineLower, "estiu"):
 			jun = append(jun, line)
 			jul = append(jul, line)
 			aug = append(aug, line)
-		case strings.Contains(strings.ToLower(line), "tardor"):
+		case strings.Contains(lineLower, "tardor"):
 			sep = append(sep, line)
 			oct = append(oct, line)
 			nov = append(nov, line)
@@ -165,17 +172,17 @@ func init() {
 		if noMonth && noSeason {
 			none = append(none, line)
 		}
-	}
 
-	// Check for any errors during the scan
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading file:", err)
+		if re.MatchString(lineLower) {
+			todays = append(todays, line)
+		}
 	}
 }
 
 func main() {
-	http.HandleFunc("/", showToday)
-	http.HandleFunc("/misc", showMisc)
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/today", handlerToday)
+	http.HandleFunc("/misc", handlerMisc)
 
 	fmt.Println("Server listening on port 8080")
 	err := http.ListenAndServe(":8080", nil)
@@ -184,11 +191,67 @@ func main() {
 	}
 }
 
-func showToday(w http.ResponseWriter, r *http.Request) {
-	var idx int
-	var resp string
+func handler(w http.ResponseWriter, r *http.Request) {
+	resp := ditaOfToday()
+	fmt.Fprintf(w, "%s", preHTML+resp+postHTML)
+}
 
-	switch int(time.Now().Month()) {
+func handlerToday(w http.ResponseWriter, r *http.Request) {
+	resp := randomDitaMonth()
+	fmt.Fprintf(w, "%s", resp)
+}
+
+func handlerMisc(w http.ResponseWriter, r *http.Request) {
+	idx := rand.IntN(len(none))
+	resp := none[idx]
+
+	fmt.Fprintf(w, "%s", resp)
+}
+
+// Return dita of today
+func ditaOfToday() string {
+	resp := ""
+	day := time.Now().Day()
+	month := int(time.Now().Month())
+	todayIs := ""
+
+	// Analyze if today is a special day like Nadal
+	switch {
+	case day == 23 && month == 4:
+		todayIs = "sant jordi"
+	case day == 24 && month == 6:
+		todayIs = "sant joan"
+	case day == 1 && month == 11:
+		todayIs = "tots sants"
+	case day == 25 && month == 12:
+		todayIs = "nadal"
+	default:
+		todayIs = "(" + fmt.Sprintf("%d", day) + " " + monthCat(month)
+	}
+
+	// Look if todayIs it is in todays. If so, build all today dites and return
+	for _, v := range todays {
+		if strings.Contains(strings.ToLower(v), todayIs) {
+			resp += v + "<br>"
+		}
+	}
+
+	if resp != "" {
+		return resp
+	}
+
+	// If it didn't return, it is because today is a normal day. Return a random dita of the month
+	return randomDitaMonth()
+}
+
+// Return random dita for the current month
+func randomDitaMonth() string {
+	idx := 0
+	resp := ""
+	month := int(time.Now().Month())
+
+	// If it didn't return, it is because today is a normal day.Return a random dita of the month
+	switch month {
 	case 1:
 		idx = rand.IntN(len(gen))
 		resp = gen[idx]
@@ -228,13 +291,37 @@ func showToday(w http.ResponseWriter, r *http.Request) {
 	default:
 		resp = ""
 	}
-
-	fmt.Fprintf(w, "%s", preHTML+resp+postHTML)
+	return resp
 }
 
-func showMisc(w http.ResponseWriter, r *http.Request) {
-	idx := rand.IntN(len(none))
-	resp := none[idx]
-
-	fmt.Fprintf(w, "%s", preHTML+resp+postHTML)
+// Translates months (1, 2...) into catalan (gener, febrer...)
+func monthCat(month int) string {
+	switch month {
+	case 1:
+		return "gener"
+	case 2:
+		return "febrer"
+	case 3:
+		return "març"
+	case 4:
+		return "abril"
+	case 5:
+		return "maig"
+	case 6:
+		return "juny"
+	case 7:
+		return "juliol"
+	case 8:
+		return "agost"
+	case 9:
+		return "setembre"
+	case 10:
+		return "octubre"
+	case 11:
+		return "novembre"
+	case 12:
+		return "desembre"
+	default:
+		return "not a month"
+	}
 }
